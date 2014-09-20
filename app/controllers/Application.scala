3d863fc2b4ba6e8b70df33e4cad9d989ecff54ac
UlisesDEV <ulisesserranomartinez@gmail.com>
2014-09-20 11:53:43 +0200

    [WIP] TASK 1.3.2 - Added boostrap for forms

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -11,35 +11,61 @@ import play.api.libs.functional.syntax._
 
 
 object Application extends Controller {
+
+	/*FORM TASKS*/
 	
 	val taskForm = Form( "label" -> nonEmptyText )
 
   	def index = Action {
-    	Redirect(routes.Application.tasks)
+    	Redirect(routes.Application.tasksForms)
   	}
 
- 	def tasks = Action {
+ 	def tasksForms = Action {
 		Ok(views.html.index(Task.all(), taskForm))
+	}	
+  
+  	def newTaskForms = Action { implicit request =>
+	  	taskForm.bindFromRequest.fold(
+	    	errors => BadRequest(views.html.index(Task.all(), errors)),
+		    label => {
+		      Task.create(label)
+		      Redirect(routes.Application.tasksForms)
+		    }
+	  	)
+	}
+
+	def deleteTaskForms(id: Long) = Action { 
+  		Task.delete(id)
+	 	Redirect(routes.Application.tasksForms)
+	}
+
+	/*API REST*/
+
+	def tasks = Action {
+		val tasks = Json.toJson(Task.all())
+		Ok(tasks)
 	}
 
 	def getTask(id: Long) = Action {
 		val task = Json.toJson(Task.getTask(id))
 		Ok(task)
 	}
-  
-  	def newTask = Action { implicit request =>
-	  	taskForm.bindFromRequest.fold(
+
+	def newTask = Action { implicit request =>
+	  	/*taskForm.bindFromRequest.fold(
 	    	errors => BadRequest(views.html.index(Task.all(), errors)),
 		    label => {
+		    	Ok("ok");
 		      Task.create(label)
 		      Redirect(routes.Application.tasks)
 		    }
-	  	)
+	  	)*/
+		Ok("ok");
 	}
   
-  	def deleteTask(id: Long) = Action { 
+  	/*def deleteTask(id: Long) = Action { 
   		Task.delete(id)
 	 	Redirect(routes.Application.tasks)
-	}
+	}*/
 
 }
