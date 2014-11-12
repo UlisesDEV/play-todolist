6bd4fe2350f901e1da3e0f79730f5d9b28e1e482
UlisesDEV <ulisesserranomartinez@gmail.com>
2014-11-12 01:44:14 +0100

    RESOLVED Tests feature 3

--- conf/evolutions/default/2.sql
+++ conf/evolutions/default/2.sql
@@ -26,6 +26,7 @@ insert into task (id,label,end,users_id) values (6,'User 2 - Tarea 3 - Id 6',NOW
 insert into task (id,label,end,users_id) values (7,'User 3 - Tarea 1 - Id 7',NOW(),3);
 insert into task (id,label,end,users_id) values (8,'User 3 - Tarea 2 - Id 8',NOW(),3);
 insert into task (id,label,users_id) values (9,'User 3 - Tarea 3 - Id 9',3);
+insert into task (id,label,end,users_id) values (10,'User 3 - Tarea 2 - Id 8','2014-11-11',3);
 # --- !Downs
  
 SET REFERENTIAL_INTEGRITY FALSE;
